3a9eee8eca931dd756f676a97bd84d17e804a7b0
Nazımhan Fıçıcı <86984378+NazimhanFicici@users.noreply.github.com>
2025-08-11 16:20:00 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,7 @@
+
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
